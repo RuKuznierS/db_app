0bf8434d7c7352bc952653716436afaf8769132a
RuKuznierS <rudineikuzniersilva@gmail.com>
2024-06-09 18:56:00 -0300

    first commit

---
+++ src/db_app/DatabaseConnection.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author COGETI
+ */
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+    
+    public static Connection getConnection(String dbName) throws SQLException {
+        return DriverManager.getConnection(URL + dbName, USER, PASSWORD);
+    }
+}
